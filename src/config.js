e8afff786967cb49ae4e12d8a62b9d72eccbe144
Adam Grant <hello@adamgrant.me>
2020-11-16 20:18:46 -0800

    Updates

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// Format: https://api.github.com/repos/{owner}/{repo}/issues/
-const GITHUB_ISSUES_LINK =
-  "https://api.github.com/repos/adamjgrant/gh-pages-url-shortener/issues/";
-
-const PATH_SEGMENTS_TO_SKIP = 0;
-
-const HOST = "adamgrant.co";
